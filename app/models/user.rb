7a35752135aea085ae14f789ead63816fdeb65ef
lingduokong <lingduokong@gmail.com>
2015-05-29 15:16:30 -0500

    finish follow

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,13 +10,19 @@ class User < ActiveRecord::Base
 										foreign_key: 'user_id',
 										dependent: :destroy
 
-	has_many :following_relationships, class_name:  "Follow",
-                                  foreign_key: "fan_id",
-                                  dependent:   :destroy
+	has_many :followships, class_name: 'Follow', foreign_key: 'star_id', dependent:   :destroy
+  has_many :followers, class_name: 'User', :through=> :followships, :source=>'fans'
 
- 	has_many :followed_relationships, class_name:  "Follow",
-                                  foreign_key: "star_id",
-                                  dependent:   :destroy                                 
+  has_many :followeeships, class_name: 'Follow', foreign_key: 'fan_id', dependent:   :destroy
+  has_many :followees, class_name: 'User', :through=> :followeeships, :source=>'stars'
+
+	# has_many :following_relationships, class_name:  "Follow",
+ #                                  foreign_key: "fan_id",
+ #                                  dependent:   :destroy
+
+ # 	has_many :followed_relationships, class_name:  "Follow",
+ #                                  foreign_key: "star_id",
+ #                                  dependent:   :destroy                                 
 
 	validates :email, :uniqueness => true, :allow_nil => false
 	validates :username, :uniqueness => true, :allow_nil => false
