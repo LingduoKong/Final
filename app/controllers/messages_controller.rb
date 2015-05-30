11aa16732a2a609742fa8a42d86861527947cb0a
lingduokong <lingduokong@gmail.com>
2015-05-30 17:22:46 -0500

    finish message

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,22 +1,26 @@
 class MessagesController < ApplicationController
 
-	def index
-		@user = User.find_by_id(session["user_id"])
+	before_action :find_connection
+	def find_connection
 		@connection = Connection.find_by_id(params["connection_id"])
-
-		if session["user_id"] == @connection.user_one_id
-			@conncetor = User.find_by_id(@connection.user_two_id)
-		else
-			@conncetor = User.find_by_id(@connection.user_one_id)
-		end
+		@connector_id =  @connection.user_one_id == session["user_id"]? @connection.user_two_id : @connection.user_one_id
 	end
 
-	def new
-		
+	def index
+		@user = User.find_by_id(session["user_id"])
+		@messages = @connection.messages.limit(100).order("date")
 	end
 
 	def create
-		
+		message = Message.new
+		message.content = params["message"]
+		message.connection_id = params["connection_id"]
+		message.sender_id = session["user_id"]
+		message.receiver_id = @connector_id
+		message.date = DateTime.now.to_i
+		if message.save
+			redirect_to connection_messages_path(params["connection_id"])
+		end
 	end
 
 	def destroy
